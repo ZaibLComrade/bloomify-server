41b7a63cd889f550a8dbb92046ace80c85740e99
ZaibLComrade <kmjahanzaib@proton.me>
2024-07-09 21:35:41 +0600

    Handled edge cases for unhandledRejection and uncaughtException

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import { connect } from "mongoose";
 import config from "./config";
 import app from "./app";
+import {Server} from "http";
 
+let server: Server;
 const main = async () => {
 	try {
 		await connect(config.db_uri as string, { dbName: config.db_name });
 		console.log("==== Connected to MongoDB ====");
 
-		app.listen(config.port, () => {
+		server = app.listen(config.port, () => {
 			console.log(`Server is running on port ${config.port}`);
 		});
 	} catch (err) {
@@ -15,3 +17,19 @@ const main = async () => {
 	}
 };
 main();
+
+
+process.on("unhandledRejection", () => {
+	console.log("Unhandled rejection detected. Shutting down...");
+	if(server !== undefined) {
+		server.close(() => {
+			process.exit(1);
+		})
+	}
+	process.exit(1)
+})
+
+process.on("uncaughtException", () => {
+	console.log("Uncaught exception detected. Shutting down");
+	process.exit(1);
+})
